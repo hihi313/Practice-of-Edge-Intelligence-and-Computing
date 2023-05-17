e174a424a888b7af2a40be3fe54176cfbb0124c0
user <M10902117@mail.ntust.edu.tw>
2023-05-17 17:50:03 +0800

    1. added log 2. update docker related files

--- superpoint_testing.py
+++ superpoint_testing.py
@@ -1,4 +1,9 @@
 
+from typing import Dict
+from openvino.runtime import Core
+from models.superpoint.models.SuperPointNet_gauss2 import SuperPointNet_gauss2
+from models.superpoint.VideoStreamer import VideoStreamer
+from models.superpoint.PointTracker import PointTracker, myjet
 import argparse
 from pathlib import Path
 import time
@@ -7,20 +12,19 @@ import cv2
 import numpy as np
 import torch
 import torch.nn.functional as F
-import sys
-print(sys.path)
-from models.superpoint.PointTracker import PointTracker, myjet
-from models.superpoint.VideoStreamer import VideoStreamer
-from models.superpoint.models.SuperPointNet_gauss2 import SuperPointNet_gauss2
-from openvino.runtime import Core
+
 
 def get_args():
     # Parse command line arguments.
     parser = argparse.ArgumentParser(description='PyTorch SuperPoint Demo.')
     parser.add_argument('input', type=str, default='',
                         help='Image directory or movie file or "camera" (for webcam).')
-    parser.add_argument('--weights_path', type=str, default='superpoint_v1.pth',
-                        help='Path to pretrained weights file (default: superpoint_v1.pth).')
+    parser.add_argument('--weights_path', type=str,
+                        help='Path to pretrained weights file.')
+    parser.add_argument('--onnx_path', type=str,
+                        help='Path to pretrained onnx file.')
+    parser.add_argument('--ir_path', type=str,
+                        help='Path to pretrained ir file.')
     parser.add_argument('--img_glob', type=str, default='*.png',
                         help='Glob match if directory of images is specified (default: \'*.png\').')
     parser.add_argument('--skip', type=int, default=1,
@@ -59,9 +63,11 @@ def get_args():
     print(opt)
     return opt
 
+
+@torch.no_grad()
 def simple_nms(scores: torch.Tensor, nms_radius: int):
     """ Fast Non-maximum suppression to remove nearby points """
-    assert(nms_radius >= 0)
+    assert (nms_radius >= 0)
 
     def max_pool(x):
         return F.max_pool2d(
@@ -76,27 +82,35 @@ def simple_nms(scores: torch.Tensor, nms_radius: int):
         max_mask = max_mask | (new_max_mask & (~supp_mask))
     return torch.where(max_mask, scores, zeros)
 
+
+@torch.no_grad()
 def remove_borders(keypoints: torch.Tensor, scores: torch.Tensor, border_width: int, height: int, width: int):
     """ Removes keypoints too close to the border """
-    mask_h = (keypoints[:, 0] >= border_width) & (keypoints[:, 0] < (height - border_width))
-    mask_w = (keypoints[:, 1] >= border_width) & (keypoints[:, 1] < (width - border_width))
+    mask_h = (keypoints[:, 0] >= border_width) & (
+        keypoints[:, 0] < (height - border_width))
+    mask_w = (keypoints[:, 1] >= border_width) & (
+        keypoints[:, 1] < (width - border_width))
     mask = mask_h & mask_w
     return keypoints[mask], scores[mask]
 
+
+@torch.no_grad()
 def select_topK(keypoints: torch.Tensor, scores: torch.Tensor, k: int):
     if k >= len(keypoints):
         return keypoints, scores
     scores, indices = torch.topk(scores, k, dim=0)
     return keypoints[indices], scores
 
-def extract_points(semi:torch.Tensor, 
-                   nms_radius: int = 4, 
+
+@torch.no_grad()
+def extract_points(semi: torch.Tensor,
+                   nms_radius: int = 4,
                    conf_thresh: float = 5e-3,
                    border_width: int = 4,
                    topK: int = -1):
     # Softmax & remove dustbin
     scores = F.softmax(semi, 1)[:, :-1]
-    b, _, h, w = scores.shape # _=64
+    b, _, h, w = scores.shape  # _=64
     # b*64*h*w -> b*h*w*64 -> b*h*w*8*8
     scores = scores.permute(0, 2, 3, 1).reshape(b, h, w, 8, 8)
     # b*h*w*8*8 -> b*h*8*w*8 -> b*(h*8)*(w*8)
@@ -105,161 +119,211 @@ def extract_points(semi:torch.Tensor,
     # NMS
     scores = simple_nms(scores, nms_radius)
     # For each batch, get location/index of keypoint
-    keypoints = [torch.nonzero(s > conf_thresh) for s in scores] # b*n_kp*2
+    keypoints = [torch.nonzero(s > conf_thresh) for s in scores]  # b*n_kp*2
     # For each batch, get score of the point by location
-    scores = [s[tuple(k.T)] for s, k in zip(scores, keypoints)] # b*n_kp
+    scores = [s[tuple(k.T)] for s, k in zip(scores, keypoints)]  # b*n_kp
 
     # Discard keypoints near the image borders
     keypoints, scores = list(zip(*[
         remove_borders(k, s, border_width, h*8, w*8)
         for k, s in zip(keypoints, scores)]))
-    
+
     # Keep the k keypoints with highest score
     if topK >= 0:
         keypoints, scores = list(zip(*[
             select_topK(k, s, topK)
             for k, s in zip(keypoints, scores)]))
-        
+
     # Convert (h, w) to (x, y)
     keypoints = [torch.flip(k, [1]).float() for k in keypoints]
     return keypoints, scores, heatmap
 
 
+@torch.no_grad()
 def extract_descriptors(keypoints, descriptors, s: int = 8):
-    """ Interpolate descriptors at keypoint locations """
-    D = descriptors.shape[1] # dim of a descriptor
     def sample(pnts, descs):
-        b, c, h, w = descs.shape  # 1*256*hc*wc, b=1
+        """ Interpolate descriptors at keypoint locations """
+        b, c, h, w = descs.shape
         pnts = pnts - s / 2 + 0.5
         pnts /= torch.tensor([(w*s - s/2 - 0.5),
-                             (h*s - s/2 - 0.5)],).to(pnts)[None]
+                              (h*s - s/2 - 0.5)],).to(pnts)[None]
         pnts = pnts*2 - 1  # normalize to (-1, 1)
         args = {'align_corners': True} if torch.__version__ >= '1.3' else {}
         descs = F.grid_sample(descs, pnts.view(b, 1, -1, 2),
                               mode='bilinear', **args)
         descs = F.normalize(descs.reshape(b, c, -1), p=2, dim=1)
-        return descs.reshape(-1, D)
-
-    return [sample(k[None], d[None])
+        return descs
+    return [sample(k[None], d[None])[0]
             for k, d in zip(keypoints, descriptors)]
 
-if __name__ == '__main__':
-    args = get_args()
 
-    # This class helps load input images from different sources.
-    vs = VideoStreamer(args.input, args.camid, args.H,
-                        args.W, args.skip, args.img_glob)
-    
-    device = torch.device('cpu')
-
-    # ONNX model
-    ONNX_PATH = Path("./models/tmp.onnx")
-    ie = Core()
-    model_onnx = ie.read_model(model=f"{ONNX_PATH}")
-    superpoint = ie.compile_model(model=model_onnx, device_name="CPU")
-    # output_layer_onnx = superpoint.output(0)
-
-    # Torch model
-    superpoint = SuperPointNet_gauss2()
-    checkpoint = torch.load(args.weights_path)
-    superpoint.load_state_dict(checkpoint["model_state_dict"])
-    superpoint.to(device)
-    superpoint.eval()
-
-    # Other config for testing
-    tracker = PointTracker(args.max_length, nn_thresh=args.nn_thresh)
-    # Create a window to display the demo.
-    win = 'SuperPoint Tracker'
-    cv2.namedWindow(win)
-    # Font parameters for visualizaton.
-    font = cv2.FONT_HERSHEY_DUPLEX
-    font_clr = (255, 255, 255)
-    font_pt = (4, 12)
-    font_sc = 0.4
-
-    while True:
-
-        start = time.time()
-
-        # Get a new image.
-        img, status = vs.next_frame()
-        if status is False:
-            break
-        # Image preprocess
-        assert img.ndim == 2, 'Image must be grayscale.'
-        assert img.dtype == np.float32, 'Image must be float32.'
-        H, W = img.shape[0], img.shape[1]
-        inp = img.copy()
-        inp = inp.reshape(1, 1, H, W)        
-
-        # Get points and descriptors.
-        start1 = time.time()
-        output = superpoint([inp])
-        val = tuple(output.values())
-        semi = val[0]
-        desc = val[1]
-        pts, scores, heatmap = extract_points(torch.tensor(semi))
-        desc = extract_descriptors(pts, torch.tensor(desc))
-        end1 = time.time()
-
-        end = time.time()
-        net_t = (1. / float(end1 - start))
-        total_t = (1. / float(end - start))
-        print(f"Processed image {vs.i} (net+post_process: {net_t:.2f} FPS, total: {total_t:.2f} FPS).")
-
-        # Get first from batch
-        pts = pts[0]
-        scores = scores[0]
-        desc = desc[0]
-        heatmap = heatmap[0]
-        # Reshape to display 
-        pts = torch.cat((pts.T, scores[None]), dim=0).numpy()
-        desc = desc.T.numpy()
-        heatmap = heatmap.numpy()
-        # Add points and descriptors to the tracker.
-        tracker.update(pts, desc)
-
-        # Get tracks for points which were match successfully across all frames.
-        tracks = tracker.get_tracks(args.min_length)
-
-        # Primary output - Show point tracks overlayed on top of input image.
-        out1 = (np.dstack((img, img, img)) * 255.).astype('uint8')
-        tracks[:, 1] /= float(args.nn_thresh)  # Normalize track scores to [0,1].
-        tracker.draw_tracks(out1, tracks)
-        cv2.putText(out1, 'Point Tracks', font_pt, font,
-                    font_sc, font_clr, lineType=16)
-
-        # Extra output -- Show current point detections.
-        out2 = (np.dstack((img, img, img)) * 255.).astype('uint8')
-        for pt in pts.T:
-            pt1 = (int(round(pt[0])), int(round(pt[1])))
-            cv2.circle(out2, pt1, 1, (0, 255, 0), -1, lineType=16)
-        cv2.putText(out2, 'Raw Point Detections', font_pt,
-                    font, font_sc, font_clr, lineType=16)
-
-        # Extra output -- Show the point confidence heatmap.
-        if heatmap is not None:
-            min_conf = 0.001
-            heatmap[heatmap < min_conf] = min_conf
-            heatmap = -np.log(heatmap)
-            heatmap = (heatmap - heatmap.min()) / \
-                (heatmap.max() - heatmap.min() + .00001)
-            
-            out3 = myjet[np.round(np.clip(heatmap*10, 0, 9)).astype('int'), :]
-            out3 = (out3*255).astype('uint8')
+def log_dict(d: Dict):
+    for k, v in d.items():
+        v = float(v)
+        if "time" in k:
+            sv = f"{v*1000:.2f}ms"
+        elif "prec" in k:
+            sv = f"{v*100:.1f}%"
         else:
-            out3 = np.zeros_like(out2)
-        cv2.putText(out3, 'Raw Point Confidences', font_pt,
-                    font, font_sc, font_clr, lineType=16)
+            sv = f"{v:.2f}"
+        sfps = f"({1./float(v):.2f} FPS)" if "total" in k else ""
+        print(f"{k}: {sv}{sfps}", end=", ")
+    print()
 
-        # Resize final output.
-        out = np.hstack((out1, out2, out3))
-        out = cv2.resize(
-            out, (3*args.display_scale*args.W, args.display_scale*args.H))
+if __name__ == '__main__':
+    with torch.no_grad():
+        args = get_args()
 
-        # Display visualization image to screen.
-        cv2.imshow(win, out)
-        key = cv2.waitKey(args.waitkey) & 0xFF
-        if key == ord('q'):
-            print('Quitting, \'q\' pressed.')
-            break
+        # This class helps load input images from different sources.
+        vs = VideoStreamer(args.input, args.camid, args.H,
+                           args.W, args.skip, args.img_glob)
+
+        device = torch.device('cpu')
+        if args.cuda:
+            device = torch.device('cuda')
+
+        superpoint = None
+        # Torch model
+        if args.weights_path:
+            superpoint = SuperPointNet_gauss2()
+            checkpoint = torch.load(args.weights_path, map_location=device)
+            superpoint.load_state_dict(checkpoint["model_state_dict"])
+            superpoint.to(device)
+            superpoint.eval()
+
+        # ONNX model
+        if args.onnx_path or args.ir_path:
+            ie = Core()
+            model_path = args.ir_path if args.onnx_path is None else args.onnx_path
+            model_onnx = ie.read_model(model=model_path)
+            superpoint = ie.compile_model(model=model_onnx, device_name="CPU")
+            # output_layer_onnx = superpoint.output(0)
+
+        # Log
+        log = {
+            "image": 0,
+            "pre_time": 0,
+            "net_time": 0,
+            "post_time": 0,
+            "total_time": 0,
+            "match_prec": 0,
+        }
+
+        # Other config for testing
+        tracker = PointTracker(args.max_length, nn_thresh=args.nn_thresh)
+        # Create a window to display the demo.
+        win = 'SuperPoint Tracker'
+        cv2.namedWindow(win)
+        # Font parameters for visualizaton.
+        font = cv2.FONT_HERSHEY_DUPLEX
+        font_clr = (255, 255, 255)
+        font_pt = (4, 12)
+        font_sc = 0.4
+
+        while True:
+
+            start = time.time()
+
+            # Preprocess
+            start_pre = time.time()
+            # Get a new image.
+            img, status = vs.next_frame()
+            if status is False:
+                break
+            assert img.ndim == 2, 'Image must be grayscale.'
+            assert img.dtype == np.float32, 'Image must be float32.'
+            H, W = img.shape[0], img.shape[1]
+            inp = img.copy()
+            inp = inp.reshape(1, 1, H, W)
+            if args.weights_path:
+                inp = torch.from_numpy(inp)
+                inp.to(device)
+            end_pre = time.time()
+
+            # Inference
+            start_net = time.time()
+            if args.weights_path:
+                output = superpoint(inp)
+            else:
+                output = superpoint([inp])
+            end_net = time.time()
+
+            # Post processing, Get points and descriptors.
+            start_post = time.time()
+            val = tuple(output.values())
+            semi = torch.tensor(val[0])
+            desc = torch.tensor(val[1])
+            pts, scores, heatmap = extract_points(semi)
+            desc = extract_descriptors(pts, desc)
+            end_post = time.time()
+
+            end = time.time()
+
+            log["image"] = vs.i
+            log["pre_time"] = end_pre - start_pre
+            log["net_time"] = end_net - start_net
+            log["post_time"] = end_post - start_post
+            log["total_time"] = end - start
+
+            # Visualization
+            # Get first from batch
+            pts = pts[0]
+            scores = scores[0]
+            desc = desc[0]
+            heatmap = heatmap[0]
+            # Reshape to display
+            pts = torch.cat((pts.T, scores[None]), dim=0).numpy()
+            desc = desc.numpy()
+            heatmap = heatmap.numpy()
+            # Add points and descriptors to the tracker.
+            matches = tracker.update(pts, desc)
+            
+            log["match_prec"] = float(matches.shape[1]) / float(pts.shape[1])
+            log_dict(log)
+
+            # Get tracks for points which were match successfully across all frames.
+            tracks = tracker.get_tracks(args.min_length)
+
+            # Primary output - Show point tracks overlayed on top of input image.
+            out1 = (np.dstack((img, img, img)) * 255.).astype('uint8')
+            # Normalize track scores to [0,1].
+            tracks[:, 1] /= float(args.nn_thresh)
+            tracker.draw_tracks(out1, tracks)
+            cv2.putText(out1, 'Point Tracks', font_pt, font,
+                        font_sc, font_clr, lineType=16)
+
+            # Extra output -- Show current point detections.
+            out2 = (np.dstack((img, img, img)) * 255.).astype('uint8')
+            for pt in pts.T:
+                pt1 = (int(round(pt[0])), int(round(pt[1])))
+                cv2.circle(out2, pt1, 1, (0, 255, 0), -1, lineType=16)
+            cv2.putText(out2, 'Raw Point Detections', font_pt,
+                        font, font_sc, font_clr, lineType=16)
+
+            # Extra output -- Show the point confidence heatmap.
+            if heatmap is not None:
+                min_conf = 0.001
+                heatmap[heatmap < min_conf] = min_conf
+                heatmap = -np.log(heatmap)
+                heatmap = (heatmap - heatmap.min()) / \
+                    (heatmap.max() - heatmap.min() + .00001)
+
+                out3 = myjet[np.round(
+                    np.clip(heatmap*10, 0, 9)).astype('int'), :]
+                out3 = (out3*255).astype('uint8')
+            else:
+                out3 = np.zeros_like(out2)
+            cv2.putText(out3, 'Raw Point Confidences', font_pt,
+                        font, font_sc, font_clr, lineType=16)
+
+            # Resize final output.
+            out = np.hstack((out1, out2, out3))
+            out = cv2.resize(
+                out, (3*args.display_scale*args.W, args.display_scale*args.H))
+
+            # Display visualization image to screen.
+            cv2.imshow(win, out)
+            key = cv2.waitKey(args.waitkey) & 0xFF
+            if key == ord('q'):
+                print('Quitting, \'q\' pressed.')
+                break
